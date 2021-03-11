83bbeb743e478b79e858b7aab8fc550d05fcab4d
dabes <daniel.dabes@gmail.com>
2021-03-11 15:20:15 -0300

    senha alterada

--- moc_data/usuario.sql
+++ moc_data/usuario.sql
@@ -1,9 +1,17 @@
 INSERT INTO
     usuario (usuario, senha, email)
 VALUES
-    ('daniel', 'leinad', 'um@email.com');
+    (
+        'daniel',
+        'cTIwODNpNDMyNGkyMzJybW5kMjNuZTIzamUybzNpZTIzMGUyMzBlIUAkMTM0ZDEzNDEzNGRjZmRzZiEjJDEyMzEyMw==',
+        'um@email.com'
+    );
 
 INSERT INTO
     usuario (usuario, senha, email)
 VALUES
-    ('daniel2', 'leinad', 'outro@email.com');
+    (
+        'daniel2',
+        'cTIwODNpNDMyNGkyMzJybW5kMjNuZTIzamUybzNpZTIzMGUyMzBlIUAkMTM0ZDEzNDEzNGRjZmRzZiEjJDEyMzEyMw==',
+        'outro@email.com'
+    );
